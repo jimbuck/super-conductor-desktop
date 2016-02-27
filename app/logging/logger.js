53451481cc50cffdaeb0dba080933faeeae07a44
Jim Buck <jim@jimmyboh.com>
2016-02-27 01:28:25 -0500

    Updated logs output directory and size limit. (Fixes #5)

--- app/logging/logger.js
+++ app/logging/logger.js
@@ -1,7 +1,15 @@
 
+import jetpack from 'fs-jetpack';
 import {Logger, transports} from 'winston';
+import electron from 'electron';
+
+let app = electron.app || (electron.remote || {}).app;
 
 import env from '../env';
+
+let logs = jetpack.cwd(app.getPath('userData')).dir('logs');
+
+const logFilePath = logs.cwd()  + '\\log.txt';
 
 const logger = new (Logger)({
   level: env === 'development' ? 'verbose' : 'info',
@@ -9,7 +17,12 @@ const logger = new (Logger)({
   humanReadableUnhandledException: true,
   transports: [
     new transports.Console(),
-    new transports.File({ filename: __dirname + '\\sc-log.txt' })
+    new transports.File({
+      filename: logFilePath,
+      tailable: true,
+      maxsize: 1024 * 1024 * 50, // 50 MB
+      maxFiles: 20
+    })
   ]
 });
 
